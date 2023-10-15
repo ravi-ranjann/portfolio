79d13707492d36e03e9223f4bef5f08fe3c8f0a9
Ravi <ravi@Ravis-MacBook-Pro.local>
2023-10-15 18:50:46 +0530

    added gh=pages

---
+++ build/precache-manifest.674630f9374bdbbb78afd4bfec2fa66d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce55c32f416cc5c86e883cf59e2dc03c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a4091185b525543b115c",
+    "url": "/portfolio/static/css/main.38adc954.chunk.css"
+  },
+  {
+    "revision": "b23f2425f67431d658ce",
+    "url": "/portfolio/static/js/2.c6fcadaf.chunk.js"
+  },
+  {
+    "revision": "bd2db5e315108ebec595",
+    "url": "/portfolio/static/js/3.5b0ae9b5.chunk.js"
+  },
+  {
+    "revision": "a4091185b525543b115c",
+    "url": "/portfolio/static/js/main.7b286fb1.chunk.js"
+  },
+  {
+    "revision": "7819eadeb6a648278ee4",
+    "url": "/portfolio/static/js/runtime~main.e59e5750.js"
+  },
+  {
+    "revision": "3f13959f67ace5f5914a948134ad5519",
+    "url": "/portfolio/static/media/Ravi_Ranjan_Resume.3f13959f.pdf"
+  },
+  {
+    "revision": "6920fdb249aa2aa786de1d317fa9fe06",
+    "url": "/portfolio/static/media/profile-pic.6920fdb2.jpeg"
+  }
+]);
